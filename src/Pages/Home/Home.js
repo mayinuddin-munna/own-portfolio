87dc46ae045b9d1b421f2a9004e9f04b67bf3827
Mayinuddin Munna <mayinuddin.munna@gmaile.com>
2022-10-18 13:08:13 +0600

    home is all done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Typed from 'react-typed';
-import { Link } from 'react-router-dom';
 import Contact from '../Contact/Contact';
-import canva from '../../presets/images/canva.png';
+import canvas from '../../presets/images/download.png';
 import bgHome from '../../presets/images/bgHome.svg';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { BsFillFileEarmarkTextFill } from 'react-icons/bs';
-
 const About = React.lazy(() => import('../About/About'));
 const Service = React.lazy(() => import('../Service/Service'));
 
@@ -16,27 +14,26 @@ const Home = () => {
             <div style={{ backgroundColor: '#061117' }}>
                 <div className='fle justify-evenly hero min-h-screen sm:flex px-6'>
                     <div className='mt-[-14rem] text-white '>
-                        <p className="text-xl mb-5"><span className='[#000]'>Hello</span> i am</p>
+                        <p className="text-xl mb-5">Hello i am</p>
                         <Typed
                             className='text-2xl sm:text-5xl tracking-widest leading-loose font-bold'
                             strings={['Mayinuddin Munna.']}
                             typeSpeed={40}
                         />
-                        <br />
                         <h1 className="text-xl sm:text-2xl font-medium mt-5">
                             Native Design & Front-End developer
                         </h1>
                         <div className="d-flex flex gap-4 mt-5 py-5 ">
                             <a href="https://www.linkedin.com/in/mayinuddin-munna-b8b9541a1/" target="_blank" rel="noreferrer" className="avatar"><FaLinkedin /></a>
                             <a href="https://github.com/munnahosssain" target="_blank" rel="noreferrer" className=""><FaGithub /></a>
-                            <a href="https://github.com/munnahosssain" target="_blank" rel="noreferrer" className="" ><BsFillFileEarmarkTextFill /></a>
+                            <a href="https://docs.google.com/document/d/1tq4DfzJRoj027lJkkN6W8oaWZASBhfw_Iex119ehRAw/edit?usp=sharing" target="_blank" rel="noreferrer" className="" ><BsFillFileEarmarkTextFill /></a>
                         </div>
                     </div>
                     <div className='mt-[-14rem] hidden lg:block'>
                         <img src={bgHome} alt='database' className="max-w-sm rounded-lg shadow-2xl" />
                     </div>
                 </div>
-                <img className='mt-[-285px]' src={canva} alt="bottom" />
+                <img className='mt-[-310px]' src={canvas} alt="bottom" />
             </div>
             <About />
             <Service />
--- src/Pages/Portfolio/Design.js
+++ src/Pages/Portfolio/Design.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 import { TbWorld } from 'react-icons/tb';
 import { AiFillGithub } from 'react-icons/ai';
-import Loading from '../Shared/Loading';
+import React, { useEffect, useState } from 'react';
 
 const Design = () => {
     const [loading, setLoading] = useState(false);
@@ -17,16 +16,16 @@ const Design = () => {
             setLoading(false);
         }, 800);
     }, []);
-    
+
     return (
-        <div className=''>
+        <div>
             <p>Design</p>
             <div className="flex grid gap-y-8 gap-x-8 lg:grid-cols-3 md:grid-cols-2 mt-10">
                 {
                     loading ? (<Loading />) : (
                         data.map((item, index) =>
                             <div key={index}>
-                                <figure className="w-92 md:92 rounded-xl p-8 dark:bg-slate-100">
+                                <figure className="w-94 rounded-xl p-8 dark:bg-slate-100">
                                     <img className="w-24 h-24 rounded-full mx-auto" src={item.img} alt="" width="384" height="512" />
                                     <div className="p-6 text-center space-y-4">
                                         <h3>{item.title}</h3>
