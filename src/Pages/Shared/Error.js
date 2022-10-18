740c6666703a38ad16bd8364a394d5ed0b590428
Mayinuddin Munna <mayinuddin.munna@gmaile.com>
2022-10-18 21:46:36 +0600

    404 home button added

--- src/Pages/Shared/Error.js
+++ src/Pages/Shared/Error.js
@@ -1,9 +1,16 @@
+import './Button.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            <p>404!</p>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md justify-self-center">
+                    <h1 className="text-4xl font-bold uppercase mb-5">404!</h1>
+                    <Link to='/' className="button btn-wrapping w-48">Go to home</Link>
+                </div>
+            </div>
         </div>
     );
 };
