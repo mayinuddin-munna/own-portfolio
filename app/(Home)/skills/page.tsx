dfea7932ec3e275325d05b8ad99cfa86a05d7558
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-07-23 22:05:52 +0600

    adsfasdfg

--- app/(Home)/skills/page.tsx
+++ app/(Home)/skills/page.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Skills: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <Button variant={"outline"}>
-        <a href="https://github.com/mayinuddin-munna" target="_blank">
-          Skills
-        </a>
-      </Button>
+      
     </div>
   );
 };
