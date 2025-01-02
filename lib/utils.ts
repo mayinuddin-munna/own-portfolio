0a0d35f5b07ed64fe074ad67dc9e24000be53cf5
mayinuddin <mayinuddin.munna@gmail.com>
2025-01-03 00:35:43 +0600

    start nextjs tech.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const truncateText = (text: string, maxLength: number): string => {
+    if (text.length <= maxLength) {
+        return text;
+    }
+    return text.substring(0, maxLength) + '...';
+};
